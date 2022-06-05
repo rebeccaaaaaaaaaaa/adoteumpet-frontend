3c7ecbbc22ae6a1f959b788d5a7e0b5b5ccda02f
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-06-05 01:23:06 -0300

    feat: adicionado title no next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,27 @@
-import '../ui/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material';
-import tema from '../ui/themes/tema';
-import Cabecalho from '../ui/components/Cabecalho/Cabecalho'
-import CabecalhoAdmin from '../ui/components/CabecalhoAdmin/CabecalhoAdmin'
-import { useRouter } from 'next/router';
+import "../ui/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material";
+import tema from "../ui/themes/tema";
+import Cabecalho from "../ui/components/Cabecalho/Cabecalho";
+import CabecalhoAdmin from "../ui/components/CabecalhoAdmin/CabecalhoAdmin";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <ThemeProvider theme={tema} >
-        {router.pathname === '/' ? <Cabecalho /> : <CabecalhoAdmin />}
+    <>
+      <Head>
+        <title> Adote um pet - Seu amigo a um clique de distância </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <ThemeProvider theme={tema}>
+        {router.pathname === "/" ? <Cabecalho /> : <CabecalhoAdmin />}
         <Component {...pageProps} />
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
-export default MyApp
+export default MyApp;
